5a45b121af9b4ddd82b737be1c3ee03e3c4b6821
nyo <nyo@users.noreply.github.com>
2018-06-04 11:53:21 +0200

    added npm updates to auto_update file

--- Debian/auto_update.sh
+++ Debian/auto_update.sh
@@ -4,3 +4,6 @@ echo "\n[ $(date) ]" >> $LOGFILE
 sudo apt-get -y update >> $LOGFILE
 echo "\n[ $(date) ]" >> $LOGFILE
 sudo apt-get -y upgrade >> $LOGFILE
+echo "\n[ $(date) ]" >> $LOGFILE
+sudo npm update >> $LOGFILE
+sudo npm update -g >> $LOGFILE
