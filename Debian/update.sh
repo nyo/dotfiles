76ad93b192bf51770596c2c5f3bca3d22ffad7a1
nyo <nyo@users.noreply.github.com>
2018-07-23 17:38:57 +0200

    fixed broken stderr redirections

--- Debian/update.sh
+++ Debian/update.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 LOGFILE="/var/log/updates.log"
-echo "\n[ $(date) ]" >> $LOGFILE
-sudo apt-get -y update 2>&1 >> $LOGFILE
-echo "\n[ $(date) ]" >> $LOGFILE
-sudo apt-get -y upgrade 2>&1 >> $LOGFILE
-echo "\n[ $(date) ]" >> $LOGFILE
-sudo apt-get -y autoremove 2>&1 >> $LOGFILE
-sudo apt-get -y autoclean 2>&1 >> $LOGFILE
+echo "\n[ $(date) ]" >>$LOGFILE
+sudo apt-get -y update 1>>$LOGFILE 2>>$LOGFILE
+echo "\n[ $(date) ]" >>$LOGFILE
+sudo apt-get -y upgrade 1>>$LOGFILE 2>>$LOGFILE
+echo "\n[ $(date) ]" >>$LOGFILE
+sudo apt-get -y autoremove 1>>$LOGFILE 2>>$LOGFILE
+sudo apt-get -y autoclean 1>>$LOGFILE 2>>$LOGFILE
